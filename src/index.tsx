0e96607eb3241ce7abece7115f7fa7253f6d0f41
Constantin <constantintofan85@gmail.com>
2024-07-18 18:25:17 +0200

    test deployment - change bundle js path

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,17 @@ import { Provider } from 'react-redux';
 import App from './containers/App';
 import store from './store/store';
 
-const container = document.getElementById('root');
-const root = createRoot(container!); // Create root
+// Ensure the root element exists
+const rootElement = document?.getElementById('root');
+if (!rootElement) {
+  throw new Error("Root element with id 'root' not found");
+}
 
+// // Set the app element for accessibility purposes
+// Modal.setAppElement('#root');
+
+// Create the root and render the application
+const root = createRoot(rootElement);
 root.render(
   <Provider store={store}>
     <App />
