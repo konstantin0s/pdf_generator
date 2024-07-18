a9bb85dd31ae9ca04fae2e5be6b273cf070a6f86
Constantin <constantintofan85@gmail.com>
2024-07-18 22:12:36 +0200

    upadte package

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-import("./bootstrap");
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable no-empty-function */
+import * as bootstrap from './bootstrap';
+
+(bootstrap as any)(() => { });
