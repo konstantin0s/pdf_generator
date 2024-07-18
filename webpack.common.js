5983941049105db47b0a54e96b77b7623a466def
Constantin <constantintofan85@gmail.com>
2024-07-18 23:14:14 +0200

    upadte package

--- webpack.common.js
+++ webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './dist/index.html',
+      template: './src/index.html',
       filename: 'index.html',
     }),
   ],
