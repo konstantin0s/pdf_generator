78b75167b19612ebea4047de4506bd10e5465387
Constantin <constantintofan85@gmail.com>
2024-07-18 22:24:26 +0200

    upadte package

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,9 @@ module.exports = {
     [MAIN_FILE_NAME]: path.join(__dirname, 'src', 'index.ts'),
   },
   output: {
-    path: path.resolve(__dirname, 'dist'), // Ensure the dist output path is set to 'dist'
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
