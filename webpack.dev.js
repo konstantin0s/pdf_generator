5dc8bde1f11fc06837907315563eb38b335aa278
Constantin <constantintofan85@gmail.com>
2024-07-18 22:42:38 +0200

    upadte package

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,36 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/pdf_generator/', // Ensure this matches your GitHub repository name
+  },
   devServer: {
+    // static: {
+    //   directory: path.join(__dirname, 'dist'),
+    // },
     static: {
       directory: path.resolve(__dirname, 'dist'),
+      staticOptions: {},
+      // Don't be confused with `devMiddleware.publicPath`, it is `publicPath` for static directory
+      // Can be:
+      // publicPath: ['/static-public-path-one/', '/static-public-path-two/'],
+      publicPath: '',
+      // Can be:
+      // serveIndex: {} (options for the `serveIndex` option you can find https://github.com/expressjs/serve-index)
+      serveIndex: true,
+      // Can be:
+      // watch: {} (options for the `watch` option you can find https://github.com/paulmillr/chokidar)
+      watch: true,
     },
     compress: true,
     port: 3000,
     open: true,
     hot: true,
-    historyApiFallback: true,
   },
 });
