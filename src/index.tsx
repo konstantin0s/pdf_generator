f576a5ddf368bdfc58950a350573b2d2055dfc00
Constantin <constantintofan85@gmail.com>
2024-07-18 19:03:07 +0200

    upadte package

--- src/index.tsx
+++ src/index.tsx
@@ -4,17 +4,9 @@ import { Provider } from 'react-redux';
 import App from './containers/App';
 import store from './store/store';
 
-// Ensure the root element exists
-const rootElement = document?.getElementById('root');
-if (!rootElement) {
-  throw new Error("Root element with id 'root' not found");
-}
+const container = document.getElementById('root');
+const root = createRoot(container!); // Create root
 
-// // Set the app element for accessibility purposes
-// Modal.setAppElement('#root');
-
-// Create the root and render the application
-const root = createRoot(rootElement);
 root.render(
   <Provider store={store}>
     <App />
